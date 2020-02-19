e2176e82eb5b8372c62f4fbb2c877b50f15c87e3
Jacky Feng <jackyfeng530@yahoo.com>
2020-02-19 18:25:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  let index = 0
+  let min = array[0]
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i]
+      index = i
+    }
+  }
+  array.splice(index, 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let min
+  let sorted = []
+  while (array.length !== 0) {
+    min = findMinAndRemove(array)
+    sorted.push(min)
+  }
+  return sorted
 }
